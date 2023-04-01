a1393b578839a22e95d6e11ba31d083d148f69d1
vikissj5 <vikissj5@gmail.com>
2023-04-01 12:48:55 +0530

    correct

--- checkout.js
+++ checkout.js
@@ -14,10 +14,19 @@ document.getElementById('ticprice').innerText = `Price : Rs ${ticketPrice}`;
 let price1 = Number.parseInt(ticketPrice);
 
 const numberOfTicketButton = document.getElementById('numTick');
-numberOfTicketButton.addEventListener('keyup', (e) => {
+numberOfTicketButton.addEventListener('change', (e) => {
     e.preventDefault();
     
     const st = subtotal(nT.value);
     document.getElementById('subTotal').innerHTML = `<div>Subtotal :   Rs ${st}</div>`;
 })
+function subtotal(ticketNumber){
 
+     const subt = Number.parseInt(Number.parseInt(ticketNumber)*price1 + price1*0.0175);
+     if(subt){
+        return subt;
+     }
+     else{
+        return 0;
+     }
+}
